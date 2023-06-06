41b8921265ba9f7842063273e32d0b47e8ae26e5
TALEX1995 <tanase.alex@libero.it>
2023-06-06 17:52:20 +0200

    Added prompt and function for random number

--- script/Script2.js
+++ script/Script2.js
@@ -1,1 +1,21 @@
 console.log('JS OK')
+
+
+// User Chose odd or even and number
+
+const oddEvenUser = prompt('Scegli pari o dispari');
+
+const userNumber = parseInt(prompt('Scegli un numero da 1 a 5'));
+
+
+// Generate random pc number
+
+const max = 5
+const min = 1
+
+// Function for random Number
+function randomNumber (max, min) {
+    const PcNumber = Math.floor(Math.random() * (max - min + 1)) + min
+    return PcNumber
+}
+
