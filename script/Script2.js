409162213cb4c6598b14186da5be36fecf279cae
TALEX1995 <tanase.alex@libero.it>
2023-06-06 18:13:40 +0200

    Added function for oddEven Number

--- script/Script2.js
+++ script/Script2.js
@@ -21,10 +21,26 @@ function randomNumber (min, max) {
 
 // Random Pc Number
 const randomPcNumber = randomNumber(min, max);
+console.log(randomPcNumber + ' random pc number')
 
 // Sum user number and pc number
 const sumUserPcNumber = userNumber + randomPcNumber;
+console.log(sumUserPcNumber + ' sum number')
 
+// Function to determinate if the number is odd or even
+function isEven (number) {
+    let evenOrOdd = 'pari'
+    if (number % 2) {
+        evenOrOdd = 'dispari'
+    }
+
+    return evenOrOdd
+}
+
+// Create a variable to save the result
+const evenOrOdd = isEven(sumUserPcNumber)
+
+// Condition to see who won
 
 
 
